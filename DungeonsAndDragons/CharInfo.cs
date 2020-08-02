de9411ead3ae4906e1f08b8b804c5b389eebd421
Obe <o.bdondo@gmail.com>
2020-08-02 15:27:21 -0700

    Added CharInfo class and CharStat class

---
+++ DungeonsAndDragons/CharInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonsAndDragons
+{
+    class CharInfo
+    {
+        
+        public int CharacterId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string CharacterName { get; set; }
+
+        public string Class { get; set; }
+
+        public int Level { get; set; }
+
+        public string Race { get; set; }
+
+        public string Background { get; set; }
+
+        public string Alignment { get; set; }
+    }
+}
---
+++ DungeonsAndDragons/CharStats.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonsAndDragons
+{
+    class CharStats
+    {
+        public int CharacterId { get; set; }
+
+        public int Strength { get; set; }
+
+        public int Dexterity { get; set; }
+
+        public int Constitution { get; set; }
+
+        public int Intelligence { get; set; }
+
+        public int Wisdom { get; set; }
+
+        public int Charisma { get; set; }
+    }
+}
