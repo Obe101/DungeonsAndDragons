38e4968661397057229876c5fc926db7f880466d
danhlogan <logan0839@student.cptc.edu>
2020-08-05 19:08:32 -0700

    Created Add New Character Form

---
+++ DungeonsAndDragons/AddNewCharacter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DungeonsAndDragons
+{
+    public partial class AddNewCharacter : Form
+    {
+        public AddNewCharacter()
+        {
+            InitializeComponent();
+        }
+
+        private void AddNewCharacter_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
