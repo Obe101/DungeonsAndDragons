c87889743db597f85a43a2f33e763e4bea014dc4
Obe <o.bdondo@gmail.com>
2020-08-07 00:17:05 -0700

    Added code to add character form

--- DungeonsAndDragons/AddNewCharacter.cs
+++ DungeonsAndDragons/AddNewCharacter.cs
@@ -21,5 +21,88 @@ namespace DungeonsAndDragons
         {
 
         }
+        private void AddCharacterBTN_Click(object sender, EventArgs e)
+        {
+            CharInfo c = new CharInfo();
+            CharStat s = new CharStat();
+
+            c.CharacterName = CharNameTXT.Text;
+            c.PlayerName = PlayerNameTXT.Text;
+            c.Class = Convert.ToString(ClassCBX.SelectedItem);
+            c.Level = Convert.ToInt32(LevelNUM.Value);
+            c.Race = Convert.ToString(RaceCBX.SelectedItem);
+            c.Alignment = getEthics() + " " + getMorals();
+            c.Background = BackgroundTXT.Text;
+
+            s.Charisma = Convert.ToInt32(CharNUM.Value);
+            s.Constitution = Convert.ToInt32(ConNUM.Value);
+            s.Dexterity = Convert.ToInt32(DexNUM.Value);
+            s.Intelligence = Convert.ToInt32(IntNUM.Value);
+            s.Strength = Convert.ToInt32(StrNUM.Value);
+            s.Wisdom = Convert.ToInt32(WisNUM.Value);
+
+            CharacterDB.Add(c);
+            CharacterDB.Add(s);
+        }
+
+        private void CharNameTXT_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RaceCBX_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LevelNUM_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+        private string getEthics()
+        {
+
+            if (EthLawfulRBTN.Checked)
+            {
+                return "Lawful";
+            }
+            else if (EthNeutralRBTN.Checked)
+            {
+                return "Neutral";
+            }
+            else if (EthChaoticRBTN.Checked)
+            {
+                return "Chaotic";
+            }
+            else
+            {
+                return null;
+            }
+        }
+        private string getMorals()
+        {
+
+            if (MorEvilRBTN.Checked)
+            {
+                return "Evil";
+            }
+            else if (MorNeutralRBTN.Checked)
+            {
+                return "Neutral";
+            }
+            else if (MorGoodRBTN.Checked)
+            {
+                return "Good";
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void CharNUM_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
