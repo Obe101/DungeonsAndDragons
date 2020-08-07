e3a2e491fae24da1c7039330317f5d3fb86b9841
Obe <o.bdondo@gmail.com>
2020-08-06 20:21:01 -0700

    Created Character DB

---
+++ DungeonsAndDragons/Migrations/202008070313512_Initial.cs
@@ -0,0 +1,47 @@
+﻿namespace DungeonsAndDragons.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CharInfoes",
+                c => new
+                    {
+                        CharacterId = c.Int(nullable: false, identity: true),
+                        PlayerName = c.String(),
+                        CharacterName = c.String(),
+                        Class = c.String(),
+                        Level = c.Int(nullable: false),
+                        Race = c.String(),
+                        Background = c.String(),
+                        Alignment = c.String(),
+                    })
+                .PrimaryKey(t => t.CharacterId);
+            
+            CreateTable(
+                "dbo.CharStats",
+                c => new
+                    {
+                        CharacterId = c.Int(nullable: false, identity: true),
+                        Strength = c.Int(nullable: false),
+                        Dexterity = c.Int(nullable: false),
+                        Constitution = c.Int(nullable: false),
+                        Intelligence = c.Int(nullable: false),
+                        Wisdom = c.Int(nullable: false),
+                        Charisma = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.CharacterId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.CharStats");
+            DropTable("dbo.CharInfoes");
+        }
+    }
+}
